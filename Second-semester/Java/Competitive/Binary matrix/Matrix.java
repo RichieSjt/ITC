37e43bdf7e8417376a836f620c9d3e2e8a70a2da
Richie_Sjt <richi252012@hotmail.com>
2019-03-05 22:05:36 -0600

    Matrix

---
+++ Second-semester/Java/Competitive/Binary matrix/Matrix.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Matrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int matrixSize, i;
+        String matrixRow;
+        boolean rowCheck = true;
+        boolean columnCheck;
+
+        do{
+            matrixSize = Integer.parseInt(sc.nextLine());
+
+            for(i = 0; i < matrixSize; i++){
+                matrixRow = sc.nextLine();
+                rowCheck &= sumRow(matrixRow);
+                sumColumn(matrixRow, matrixSize);
+            }
+            if(rowCheck == true)
+                System.out.println("OK");
+            else
+                System.out.println("Corrupt");
+
+            rowCheck = true;
+        }while(matrixSize != 0);
+        
+    }
+    public static boolean sumRow(String row){
+        String[] n;
+        int sum = 0, a;
+
+        n = row.split(" ");
+
+        for(String x: n){
+            a = Integer.parseInt(x);
+            sum += a;
+        }
+        return (sum%2 == 0);
+
+    }
+    public static int sumColumn(String column, int matrixSize){
+        for(i = 0; )
+    }
+}
