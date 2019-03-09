d6a5f96e04dee32085e914a3f37a7b009773e792
Richie_Sjt <richi252012@hotmail.com>
2019-03-08 18:32:46 -0600

    Services activity completed

--- Second-semester/Java/Object Oriented Programming/Communication Services/CSPFactory.java
+++ Second-semester/Java/Object Oriented Programming/Communication Services/CSPFactory.java
@@ -19,56 +19,62 @@ public class CSPFactory {
         TelephoneFixedCost tel = new TelephoneFixedCost();
         tel.basePrice = 20;
         att.addService(tel);
+
         return att;
     }
 
     public CSP createAxtel(String media, double usedMb) {
-        /*
         CSP axtel = new CSP();
         axtel.name = "Axtel";
-    
-        InternetConnection ic = new InternetConnection();
-        ic.basePrice = 40;
-        ic.baseMb = 50;
-        ic.costPerMb = 2;
-        ic.usedMb = usedMb;
-        axtel.addService(ic);
-    
-        PaidTV tv = new PaidTV();
-        tv.basePrice = 40;
-        tv.costPerChannel = 10;
-        tv.channels = channels;
-        axtel.addService(tv);
         
-        TelephoneFixedCost tel = new TelephoneFixedCost();
-        tel.basePrice = 20;
-        axtel.addService(tel);
-        return axtel;  */
-        return null; 
+        if(media.equals("optic")){
+
+            InternetConnection optic = new InternetConnection();
+            optic.basePrice = 350;
+            optic.baseMb = 20;
+            optic.costPerMb = 1;
+            optic.usedMb = usedMb;
+            axtel.addService(optic);
+
+        }else if(media.equals("satellite")){
+
+            InternetConnection satellite = new InternetConnection();
+            satellite.basePrice = 5000;
+            satellite.baseMb = 10;
+            satellite.costPerMb = 10;
+            satellite.usedMb = usedMb;
+            axtel.addService(satellite);
+        }
+        
+        TelephoneFixedCost unlimitedNational = new TelephoneFixedCost();
+        unlimitedNational.basePrice = 20;
+        axtel.addService(unlimitedNational);
+
+        return axtel;
     }
 
-    public CSP createInfinitum(int calls, double usedMmb, int cellMins) {
-        /*
+    public CSP createInfinitum(int calls, double usedMb, int cellMins) {
+        
         CSP infinitum = new CSP();
         infinitum.name = "Infinitum";
-    
-        InternetConnection ic = new InternetConnection();
-        ic.basePrice = 40;
-        ic.baseMb = 50;
-        ic.costPerMb = 2;
-        ic.usedMb = usedMb;
-        infinitum.addService(ic);
-    
-        PaidTV tv = new PaidTV();
-        tv.basePrice = 40;
-        tv.costPerChannel = 10;
-        tv.channels = channels;
-        infinitum.addService(tv);
         
-        TelephoneFixedCost tel = new TelephoneFixedCost();
-        tel.basePrice = 20;
-        infinitum.addService(tel);
-        return infinitum; */
-        return null;
+        CellTelephoneCall cellPhone = new CellTelephoneCall();
+        cellPhone.costPerMinute = 1;
+        cellPhone.minutes = cellMins;
+        infinitum.addService(cellPhone);
+
+        InternetConnection internet = new InternetConnection();
+        internet.basePrice = 200;
+        internet.baseMb = 10;
+        internet.costPerMb = 10;
+        internet.usedMb = usedMb;
+        infinitum.addService(internet);
+
+        CellTelephoneCall localCall = new CellTelephoneCall();
+        localCall.costPerMinute = 1;
+        localCall.minutes = calls;
+        infinitum.addService(localCall);
+
+        return infinitum;
     }
 }
--- Second-semester/Java/Object Oriented Programming/Communication Services/DemoCSP.java
+++ Second-semester/Java/Object Oriented Programming/Communication Services/DemoCSP.java
@@ -1,13 +1,17 @@
 public class DemoCSP {
     public static void main(String[] args) {
         CSPFactory factory = new CSPFactory();
-        //Client data
-        double usedMb = 100;
-        int channels = 10;
-        //ATT&T Object
-        CSP att = factory.createATT(usedMb, channels);
-        //Call the method to calculate the total cost
-        double cost = att.calculateCost();
-        System.out.println(cost);
+        
+        CSP att = factory.createATT(100, 10); //ATT&T Object
+        double cost = att.calculateCost(); //Call the method to calculate the total cost
+        System.out.println("ATT service cost: " + cost);
+
+        CSP axtel = factory.createAxtel("satellite", 150);
+        cost = axtel.calculateCost();
+        System.out.println("Axtel service cost: " + cost);
+
+        CSP infinitum = factory.createInfinitum(25, 200, 234);
+        cost = infinitum.calculateCost();
+        System.out.println("Infinitum service cost: " + cost);
     }
 }
