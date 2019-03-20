cef845a07e14f25d194b7ae03079d0d70df50ae4
Richie_Sjt <richi252012@hotmail.com>
2019-03-19 21:32:43 -0600

    Heaters problem attempt

---
+++ Second-semester/Java/Competitive/Vova house/Vova.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Vova {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfElements, i, heaterRange, heatLeft, heatRight, j, counter = 0;
+        String[] heaters;
+        String input, elements, check = "";
+
+        input = sc.nextLine();
+
+        numberOfElements = Integer.parseInt(input.substring(0, input.indexOf(" ")));
+        heaterRange = Integer.parseInt(input.substring(input.indexOf(" ") + 1));
+        heaters = new String[numberOfElements];
+
+        elements = sc.nextLine();
+        heaters = elements.split(" ");
+
+        for(i = 0; i < numberOfElements; i++){
+            if (heaters[i].equals("0"))
+                heaters[i] = "2"; 
+        }
+
+        for(i = 0; i < numberOfElements; i++) {
+            if(heaters[i].equals("1")){
+                heatLeft = i - heaterRange + 1;
+                heatRight = i + heaterRange - 1;
+                if(heatLeft < 0 /*&& heaters[i] != 1*/ ){
+                    for(j = 0; j <= heatRight; j++ ){
+                        heaters[j] = "0";
+                    }
+                }else if(heatRight >= numberOfElements){
+                    for(j = heatLeft; j < numberOfElements; j++ ){
+                        heaters[j] = "0";
+                    }
+                }else{
+                    for(j = heatLeft; j <= heatRight; j++){
+                        heaters[j] = "0";
+                    }
+                }
+                counter++;
+            }
+        }
+
+        for(i = 0; i < numberOfElements; i++) {
+            if (heaters[i].equals("2"))
+                check = "-1";
+        }
+
+        if(check.equals("-1"))
+            System.out.println("-1");
+        else
+            System.out.println(counter);
+    }
+}
