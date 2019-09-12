73333535c9c62fa8a096357646942a42acb3a5d1
RichieSjt <richi252012@hotmail.com>
2019-09-12 10:55:40 -0500

    Abstract data types

---
+++ Third-Semester/ADT/Shape.java
@@ -0,0 +1,13 @@
+public abstract class Shape {
+    public String name;
+
+    public double calculateArea;
+    public double calculatePerimeter;
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+}
