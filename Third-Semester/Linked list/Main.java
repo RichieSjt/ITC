ddaa6ed3e9e1b27d435e9db77d9743185861fa8f
Richie_Sjt <richi252012@hotmail.com>
2019-09-11 23:25:34 -0500

    Data structure exercises

---
+++ Third-Semester/Linked list/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        ListaLigada<Integer> lista = new ListaLigada<>();
+        lista.estaVacia();
+        lista.insertarAlInicio(2);
+        lista.insertarAlInicio(8);
+        lista.insertarAlInicio(7);
+        lista.insertarAlUltimo(43);
+        lista.insertarAlUltimo(69);
+        lista.imprimeLista();
+        System.out.println("Numero de elementos: " + lista.contarElementos());
+
+        lista.quitarPrimero();
+        lista.imprimeLista();
+        System.out.println("Numero de elementos: " + lista.contarElementos());
+
+        lista.quitarUltimo();
+        lista.imprimeLista();
+        System.out.println("Numero de elementos: " + lista.contarElementos());
+
+        lista.obtenerElUltimo();
+        lista.obtenerElPrimero();
+
+        lista.encontrarElementoEnIndice(2);
+        lista.encontrarElementoEnIndice(3);
+
+        lista.introducirDatoEnElIndice(19, 2);
+        lista.imprimeLista();
+    }
+}
