810aaa44014d47d913ecf2f0ca876d22f4dea06e
Richie_Sjt <richi252012@hotmail.com>
2019-11-10 18:52:35 -0600

    Fix

--- First-semester/Java code 2/Alphabeat Soup/Soup.java
+++ First-semester/Java code 2/Alphabeat Soup/Soup.java
@@ -1,36 +1,54 @@
 public class Soup{
     public static void main (String[] args){
-        String names, p;
-        int i, j; 
-        String finalsoup[][] = new String[20][20]; 
+        String names;
+        int x = 0, y = 0;
+        boolean possible = false;
+        char[] letters;
+        String finalsoup[][] = new String[20][20];
 
-        System.out.println("--Introduce 5 words to hide in the Soup separated by an space---");
+        System.out.println("--Introduce the words to hide in the Soup separated by an space---");
         names = System.console().readLine();
-        String Snames[] = names.split(" ");
+        String WordsArray[] = names.split(" ");
 
-            for(i = 0; i < Snames.length ; i++){
-                String w = Snames[i];
-                String o, coordinates;
+        for(int i = 0; i < WordsArray.length ; i++){
+            String word = WordsArray[i];
+            String orientation = "";
 
-                do{
-                    int xy[] = Positions();
-                    o = Orientation();
-                    if (o.equals("horizontal")){
-                        p = Posible(xy[0], w.length());
-                        System.out.println("It is " + p + " to hide word " + (i+1) + ".");
-                    }else{
-                        p = Posible(xy[1], w.length());
-                        System.out.println("It is " + p + " to hide word " + (i+1) + ".");
-                    }
-                    coordinates = String.valueOf(xy[0]) + " " + String.valueOf(xy[1]); 
-                    System.out.println("Coordinates: " + coordinates);
-                }while( p.equals("not posible") | p.equals("not posible") );
-            
-                finalsoup = write(o, w, coordinates);
+            //The methods generate random positions and orientations
+            //until they find valid ones to fit the word.
+
+            while(!possible){
+                int xy[] = Positions();
+                x = xy[0];
+                y = xy[1];
+
+                orientation = Orientation();
+                if (orientation.equals("horizontal"))
+                    possible = Posible(x, word.length());
+                else
+                    possible = Posible(y, word.length());
+                    
             }
 
-        for ( j = 0; j < 20 ; j++  ){
-            for ( i = 0; i < 20 ; i++ ){
+            int posx = x + word.length();
+            int posy = y + word.length();
+
+            //The word is split into letters and stored in an array
+            letters = word.toCharArray();
+
+            int j;
+            if(orientation.equals("horizontal")){
+                for(x = x, j = 0 ; x < posx && j < letters.length ; x++, j++)
+                    finalsoup[x][y] = letters[j] + ""; 
+            }else{
+                for(y = y, j = 0; y < posy && j < letters.length ; y++, j++)
+                    finalsoup[x][y] = letters[j] + "";
+            }
+        }
+
+        //Filling the missing spaces in the array and printing it
+        for (int j = 0; j < 20 ; j++){
+            for (int i = 0; i < 20 ; i++){
                 if(finalsoup[i][j] == null){
                     finalsoup[i][j] = Rletter();
                     System.out.print(finalsoup[i][j] + " ");
@@ -43,79 +61,40 @@ public class Soup{
         }
     }
 
+    //Checks if its possible to hide the word
+    public static boolean Posible(int pos, int wordlen){
+        boolean yn;
+        if(pos + wordlen <= 20){
+            yn = true;
+        }else{
+            yn = false;
+        }        
+        return yn; 
+    }
+
+    //Returns random x and y position
     public static int[] Positions(){
         int xy[] = new int [2];
         xy[0] = (int)(Math.random()*20);
         xy[1] = (int)(Math.random()*20);
         return xy;
     }
-
+    
+    //Returns a random orientation (horizontal/vertical)
     public static String Orientation(){
         String or;
         int a = (int)(Math.random()*2);
-        if (a == 0){
+        if (a == 0)
             or = "horizontal";
-        }else{
+        else
             or = "vertical";
-        }
         return or;
     }
-
-    public static String Posible(int pos, int wordlen){
-        String yn;
-        if(pos + wordlen <= 20){
-            yn = "posible";
-        }else{
-            yn = "not posible";
-        }        
-        return yn; 
-    } 
-
-    public static String[][] write(String orientation, String word, String coordinates){
-
-        String print[][] = new String[20][20];
-        char[] letters;
-        String[] Sletters = new String[word.length()];
-        int x, y, i, j;
-
-        String xy[] = coordinates.split(" ");   
-        int xyint[];
-        xyint = new int [2];
-
-        for ( i= 0; i < xy.length; i++){
-            int a = Integer.parseInt(xy[i]);
-            xyint[i] = a;
-        }
-
-        int posx = xyint[0] + word.length();
-        int posy = xyint[1] + word.length();
-
-        letters = word.toCharArray();
-        for ( i= 0; i < letters.length; i++){
-            String s = String.valueOf(letters[i]);
-            Sletters[i] = s;
-        }
-
-        if(orientation.equals("horizontal")){
-            y = xyint[1]; 
-            for(x = xyint[0], j = 0 ; x < posx && j < Sletters.length ; x++ , j++ ){
-                
-                    print[x][y] = Sletters[j]; 
-            
-            }
-        }else{
-            x = xyint[0];
-            for( y = xyint[1], j = 0;y < posy && j < Sletters.length ; y++, j++ ){
-                
-                    print[x][y] = Sletters[j];
-                
-            }
-        }
-        return print; 
-    }
     
+    //Returns a random letter
     public static String Rletter(){
-        String l; int n; 
+        int n; 
+        String l; 
         String letters[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
          "s", "t", "u", "v", "w", "x", "y", "z"};
         n = (int)(Math.random()*26);
