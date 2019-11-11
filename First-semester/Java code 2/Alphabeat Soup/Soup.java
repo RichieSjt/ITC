598f17960e3f35acd4a41734e769aa81fc620eb0
Richie_Sjt <richi252012@hotmail.com>
2019-11-10 20:01:59 -0600

    Fix

--- First-semester/Java code 2/Alphabeat Soup/Soup.java
+++ First-semester/Java code 2/Alphabeat Soup/Soup.java
@@ -1,14 +1,14 @@
 public class Soup{
     public static void main (String[] args){
-        String names;
+        String input;
         int x = 0, y = 0;
-        boolean possible = false;
+        boolean possible;
         char[] letters;
         String finalsoup[][] = new String[20][20];
 
-        System.out.println("--Introduce the words to hide in the Soup separated by an space---");
-        names = System.console().readLine();
-        String WordsArray[] = names.split(" ");
+        System.out.println("//Enter the words to hide in the Soup separated by an space//");
+        input = System.console().readLine();
+        String WordsArray[] = input.split(" ");
 
         for(int i = 0; i < WordsArray.length ; i++){
             String word = WordsArray[i];
@@ -16,41 +16,51 @@ public class Soup{
 
             //The methods generate random positions and orientations
             //until they find valid ones to fit the word.
-
+            possible = false;
             while(!possible){
-                int xy[] = Positions();
+                int xy[] = positions();
                 x = xy[0];
                 y = xy[1];
 
-                orientation = Orientation();
+                orientation = orientation();
                 if (orientation.equals("horizontal"))
-                    possible = Posible(x, word.length());
+                    possible = posible(x, word.length());
                 else
-                    possible = Posible(y, word.length());
-                    
+                    possible = posible(y, word.length());
             }
 
+            //The word is split into letters and stored in an array.
+            letters = word.toCharArray();
+
+            //x and y positions of the last letter of the word.
             int posx = x + word.length();
             int posy = y + word.length();
 
-            //The word is split into letters and stored in an array
-            letters = word.toCharArray();
-
             int j;
+            //Writing the letters into the finalSoup array depending
+            //on the words orientation.
             if(orientation.equals("horizontal")){
-                for(x = x, j = 0 ; x < posx && j < letters.length ; x++, j++)
-                    finalsoup[x][y] = letters[j] + ""; 
-            }else{
-                for(y = y, j = 0; y < posy && j < letters.length ; y++, j++)
+                j = 0;
+                while(x < posx && j < letters.length){   
                     finalsoup[x][y] = letters[j] + "";
+                    x++;
+                    j++;
+                }
+            }else{
+                j = 0;
+                while(y < posy && j < letters.length){
+                    finalsoup[x][y] = letters[j] + "";
+                    y++;
+                    j++;
+                }
             }
         }
 
-        //Filling the missing spaces in the array and printing it
+        //Filling the blank spaces in the array and then printing it.
         for (int j = 0; j < 20 ; j++){
             for (int i = 0; i < 20 ; i++){
                 if(finalsoup[i][j] == null){
-                    finalsoup[i][j] = Rletter();
+                    finalsoup[i][j] = randomLetter();
                     System.out.print(finalsoup[i][j] + " ");
                 }
                 else{
@@ -61,27 +71,27 @@ public class Soup{
         }
     }
 
-    //Checks if its possible to hide the word
-    public static boolean Posible(int pos, int wordlen){
+    //Checks if its possible to hide the word.
+    public static boolean posible(int pos, int wordlen){
         boolean yn;
-        if(pos + wordlen <= 20){
+        //TODO: Check if a position we want to fill has already been filled to avoid overwriting it.
+        if(pos + wordlen <= 20)
             yn = true;
-        }else{
-            yn = false;
-        }        
+        else
+            yn = false;  
         return yn; 
     }
 
     //Returns random x and y position
-    public static int[] Positions(){
+    public static int[] positions(){
         int xy[] = new int [2];
         xy[0] = (int)(Math.random()*20);
         xy[1] = (int)(Math.random()*20);
         return xy;
     }
     
-    //Returns a random orientation (horizontal/vertical)
-    public static String Orientation(){
+    //Returns a random orientation (horizontal/vertical).
+    public static String orientation(){
         String or;
         int a = (int)(Math.random()*2);
         if (a == 0)
@@ -91,8 +101,8 @@ public class Soup{
         return or;
     }
     
-    //Returns a random letter
-    public static String Rletter(){
+    //Returns a random letter.
+    public static String randomLetter(){
         int n; 
         String l; 
         String letters[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
